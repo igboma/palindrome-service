ba9345c6922d9d2d6aa322ff18608e6f61a3392c
Michael Igbo <igboma.michael@gmail.com>
2024-07-07 21:47:41 -0700

    update

--- app/routes/message.server.routes.js
+++ app/routes/message.server.routes.js
@@ -2,8 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
-
 const message = require('../controllers/message.server.controller');
+
 router.get('/messages', message.getMessages);
 router.post('/messages/', message.postMessage);
 router.get('/messages/:id', message.getSingleMessage);
